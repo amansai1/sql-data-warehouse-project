a305fd22b27267160cb3455c1126b787986f8bf6
Aman <45324545+amansai1@users.noreply.github.com>
2025-12-12 17:34:33 +0000

    Update ddl.bronze.sql

--- scripts/bronze/ddl.bronze.sql
+++ scripts/bronze/ddl.bronze.sql
@@ -34,7 +34,7 @@ CREATE TABLE bronze.crm_prd_info (
     prd_nm NVARCHAR ( 50),
     prd_cost INT,
     prd_line NVARCHAR (50),
-    prd_start_d DATETIME, 
+    prd_start_dt DATETIME, 
     prd_end_dt DATETIME
 );
 GO
